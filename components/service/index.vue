9c2218e156bce2954fe01f41c30c121376c2a9d2
khalil.abdelsalam <khalil.abdelsalam@tecbadia.com>
2022-03-24 01:35:40 +0200

    comment

--- components/service/index.vue
+++ components/service/index.vue
@@ -3,40 +3,37 @@
     <v-item-group>
       <v-container>
         <v-row>
-          <v-col
-            v-for="(Proudcts, i) in allProudcts.data"
-            :key="i"
-            :cols="i"
-            md="4"
-          >
+          <v-col v-for="(Proudcts,i) in allProudcts.data" :key="i"
+            :cols="i" md="4">
             <v-card class="mx-auto" max-width="344">
-              <v-img :src="Proudcts.image" height="200px"></v-img>
+              <v-img
+                 :src="Proudcts.image"
+                height="200px"
+              ></v-img>
 
-              <v-card-title> {{ Proudcts.title }} </v-card-title>
+              <v-card-title> {{Proudcts.title}} </v-card-title>
 
-              <v-card-subtitle> {{ Proudcts.description }} </v-card-subtitle>
-              <v-card-subtitle>
-                <span bold>Price:</span> {{ Proudcts.price }}
-              </v-card-subtitle>
+              <v-card-subtitle>  {{Proudcts.description}} </v-card-subtitle>
+              <v-card-subtitle> <span bold>Price:</span> {{Proudcts.price}} </v-card-subtitle>
 
               <v-card-actions>
+
                 <v-spacer></v-spacer>
-                <form @submit.prevent="onSubmit">
-                  <v-text-field
+
+                <v-btn class="mx-2" fab dark  color="indigo" @click="addToCart">
+                  <v-icon
+                    dark
+                    > mdi-cart-outline </v-icon>
+                </v-btn>
 
 
-                    v-model="prodName"
-                    :value="Proudcts.id"
-                    single-line
-                  >
-                  </v-text-field>
 
-                  <v-btn type="submit" class="mx-2" fab dark color="indigo">
-                    <v-icon dark> mdi-cart-outline </v-icon>
-                  </v-btn>
-                </form>
               </v-card-actions>
             </v-card>
+
+
+
+
           </v-col>
         </v-row>
       </v-container>
@@ -65,26 +62,24 @@
 }
 </style>
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
-  data(){
-    return {
-      prodName:null
-    }
+
+    computed:{
+    ...mapGetters(['allProudcts'])
+
   },
-  computed: {
-    ...mapGetters(['allProudcts']),
-  },
-  methods: {
+  methods:{
     ...mapActions(['getProducts', 'addToCart']),
-    onSubmit(){
-      this.addToCart(this.prodName)
-    }
+    // onSubmit(){
+    //   this.addToCart(this.prodName)
+    // }
   },
-  mounted() {
-    this.getProducts()
-    // this.addToCart()
-  },
+  mounted(){
+    this.getProducts();
+    this.addToCart();
+  }
+
 }
 </script>
